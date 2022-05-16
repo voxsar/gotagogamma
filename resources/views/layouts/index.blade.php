018bcc5701bcbf595cae07c2b82024fd8b7c69cd
Miyuru Dharmage <voxsar@gmail.com>
2022-05-16 05:30:32 +0000

    mobile fixes

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Bootstrap CSS -->
